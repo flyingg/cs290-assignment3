2eacc7083225e7efbea9395d43aef247a441100a
Gerald <galeg@onid.oregonstate.edu>
2015-01-29 22:50:20 -0800

    Finally completed at least one file for correct assertions - variables.js

--- src/variables.js
+++ src/variables.js
@@ -14,6 +14,10 @@ function variableModification(a) {
   var asString;
   var yourNumberIs;
   //your code here
+  plus5 = a + 5;
+  asString = a.toString();
+  var x = a;
+  yourNumberIs = "Your Number is " + x;
 
   //end your code
   return [plus5, asString, yourNumberIs, a];
@@ -29,7 +33,12 @@ false otherwise
 */
 function isString(b) {
   //your code here
-  return undefined;
+  if(typeof b == 'string'){
+    return true;
+  }
+  else{
+    return false;
+  }
   //end your code
 }
 
